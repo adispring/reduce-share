6ecbe546d90bcb85703a4b9297eb363fa48fb57f
wangzengdi <sunnyadi@163.com>
2017-08-26 14:10:17 +0800

    add fan

---
+++ code/reduce.js
@@ -0,0 +1,9 @@
+const reduce = (reducer, initialValue, list) => {
+  let acc = initialValue;
+  let val;
+  for(i = 0, i < list.length, i++) {
+    val = list[i];
+    acc = reducer(acc, val);
+  }
+  return acc;
+}
---
+++ code/reduceSignature.js
@@ -0,0 +1,7 @@
+reduce:: ((a, b) -> a) -> a -> [b] -> a
+reduce:: (reducer, initialValue list) -> result
+reducer:: (a, b) -> a
+initialValue:: a
+list:: [b]
+result:: a
+reduce(sum, 1, [2, 3, 4]);
